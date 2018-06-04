6ed181702cfabf8c062ca8cf610f3aacb9214d0b
SolenneD <sdaguerre75@gmail.com>
2018-06-04 10:45:19 +0200

    add Analytic et Manager + SEO + host 1&1

--- config/settings.php
+++ config/settings.php
@@ -1,14 +1,24 @@
 <?php
 session_start();
 
-define("SQL_HOST","localhost");
-define("SQL_USER", "root");
-define("SQL_PASS", "root");
-define("SQL_DBNAME", "idagency");
+if($_SERVER['SERVER_NAME']== 'localhost') {
+    define("SQL_HOST", "localhost");
+    define("SQL_USER", "root");
+    define("SQL_PASS", "root");
+    define("SQL_DBNAME", "idagency");
+}
+
+else{
+    define("SQL_HOST","db740498105.db.1and1.com");
+    define("SQL_USER","dbo740498105");
+    define("SQL_PASS","Iesa2018!");
+    define("SQL_DBNAME","db740498105");
+}
+
 
 try{
     $db = new PDO("mysql:dbname=".SQL_DBNAME.";charset=utf8;host=".SQL_HOST,SQL_USER,SQL_PASS);
-}catch(Execption $e){
+}catch(Exception $e){
     die('Erreur : ' . $e->getMessage());
 }
 
--- index.php
+++ index.php
@@ -5,16 +5,49 @@ include('config/settings.php');
 ?><!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120265874-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-120265874-1');
+    </script>
+    <!--  Google Analytics  -->
+    <script>
+        dataLayer = [{
+            'pageCategory': 'signup',
+            'visitorType': 'high-value'
+        }];
+    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KPP7XMR');</script>
+    <!-- End Google Tag Manager -->
+
+
+
+    <title>Paceme - Laissez votre corps guider la musique</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Synchronisez passion musicale et plaisir infini avec votre nouvelle bague connectée">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPP7XMR"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+
 	<header>
 		<div class="logo">
-			<img src="">
+			<img src="" alt="logo-paceme">
 		</div>
 	</header>
 	
